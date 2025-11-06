9b4ffe6b6834a5349301baf76c8758fd72eb79b6
Matthew Bowen <matthew.bowen@abs.uk.com>
2025-11-06 17:09:17 +0000

    ABS-28 Added a unit test for get total price before implementing the code

--- CheckoutKata.Model.Test/DomainServices/CheckoutServiceTest.cs
+++ CheckoutKata.Model.Test/DomainServices/CheckoutServiceTest.cs
@@ -13,6 +13,38 @@ namespace CheckoutKata.Model.Test.DomainServices;
 /// </summary>
 public class CheckoutServiceTest
 {
+    #region GetTotalPrice
+
+    /// <summary>
+    ///     Confirms that when GetTotalPrice is called.  It calls the mediator to processes discounts
+    /// </summary>
+    [Fact]
+    public async Task CheckoutService_GetTotalPrice_CallsMediatorWithTheDiscount()
+    {
+        var firstProductItem = new ProductItem
+        {
+            Sku = "A",
+            UnitPrice = 50.00m
+        };
+        var mediatorMock = new Mock<IMediator>();
+        var discountRepositoryMock = new Mock<IDiscountRepository>();
+        var discountRequest = new BuyXOnePriceDiscount();
+        discountRepositoryMock.Setup(d => d.GetDiscountsAsync()).ReturnsAsync(new List<IRequest<DiscountRequest>>
+            { discountRequest });
+        var productItemRepositoryMock = new Mock<IProductItemRepository>();
+
+        productItemRepositoryMock.Setup(p => p.GetProductItemAsync(It.IsAny<string>())).ReturnsAsync(firstProductItem);
+        var checkoutService = new CheckoutService(mediatorMock.Object, discountRepositoryMock.Object,
+            productItemRepositoryMock.Object);
+        await checkoutService.ScanAsync("A");
+        checkoutService.GetTotalPrice();
+        mediatorMock.Verify(m => m.Send(It.IsAny<BuyXOnePriceDiscount>(), It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    #endregion
+
+    #region ScanAsync
+
     /// <summary>
     ///     Confirm that the first time round a scan is called, it gets the discounts
     /// </summary>
@@ -23,7 +55,8 @@ public class CheckoutServiceTest
         var discountRepositoryMock = new Mock<IDiscountRepository>();
         var productItemRepositoryMock = new Mock<IProductItemRepository>();
         productItemRepositoryMock.Setup(p => p.GetProductItemAsync(It.IsAny<string>())).ReturnsAsync(new ProductItem());
-        var checkoutService = new CheckoutService(It.IsAny<IMediator>(), discountRepositoryMock.Object, productItemRepositoryMock.Object);
+        var checkoutService = new CheckoutService(It.IsAny<IMediator>(), discountRepositoryMock.Object,
+            productItemRepositoryMock.Object);
         await checkoutService.ScanAsync(sku);
         discountRepositoryMock.Verify(d => d.GetDiscountsAsync(), Times.Once);
     }
@@ -41,7 +74,8 @@ public class CheckoutServiceTest
             { new BuyXOnePriceDiscount() });
         var productItemRepositoryMock = new Mock<IProductItemRepository>();
         productItemRepositoryMock.Setup(p => p.GetProductItemAsync(It.IsAny<string>())).ReturnsAsync(new ProductItem());
-        var checkoutService = new CheckoutService(It.IsAny<IMediator>(), discountRepositoryMock.Object,productItemRepositoryMock.Object);
+        var checkoutService = new CheckoutService(It.IsAny<IMediator>(), discountRepositoryMock.Object,
+            productItemRepositoryMock.Object);
         await checkoutService.ScanAsync(sku1);
         await checkoutService.ScanAsync(sku2);
         discountRepositoryMock.Verify(d => d.GetDiscountsAsync(), Times.Once);
@@ -58,14 +92,15 @@ public class CheckoutServiceTest
             Sku = "A",
             UnitPrice = 50.00m
         };
-        
+
         var productItemRepositoryMock = new Mock<IProductItemRepository>();
         productItemRepositoryMock.Setup(p => p.GetProductItemAsync(It.IsAny<string>())).ReturnsAsync(productItem);
-        var checkoutService = new CheckoutService(Mock.Of<IMediator>(), Mock.Of<IDiscountRepository>(), productItemRepositoryMock.Object);
+        var checkoutService = new CheckoutService(Mock.Of<IMediator>(), Mock.Of<IDiscountRepository>(),
+            productItemRepositoryMock.Object);
         await checkoutService.ScanAsync("A");
         productItemRepositoryMock.Verify(p => p.GetProductItemAsync("A"), Times.Once);
     }
-    
+
     /// <summary>
     ///     Confirms that if an item is not returned from the repository when scanned then the correct exception is thrown
     /// </summary>
@@ -73,8 +108,12 @@ public class CheckoutServiceTest
     public async Task CheckoutService_ScanAsync_ProductNotFound_CorrectExceptionThrown()
     {
         var productItemRepositoryMock = new Mock<IProductItemRepository>();
-        var checkoutService = new CheckoutService(Mock.Of<IMediator>(), Mock.Of<IDiscountRepository>(), productItemRepositoryMock.Object);
-        productItemRepositoryMock.Setup(p => p.GetProductItemAsync(It.IsAny<string>())).ReturnsAsync((ProductItem?)null);
-        await Assert.ThrowsAsync<ProductNotFoundException>(async () =>await checkoutService.ScanAsync("A") );
+        var checkoutService = new CheckoutService(Mock.Of<IMediator>(), Mock.Of<IDiscountRepository>(),
+            productItemRepositoryMock.Object);
+        productItemRepositoryMock.Setup(p => p.GetProductItemAsync(It.IsAny<string>()))
+            .ReturnsAsync((ProductItem?)null);
+        await Assert.ThrowsAsync<ProductNotFoundException>(async () => await checkoutService.ScanAsync("A"));
     }
+
+    #endregion
 }
