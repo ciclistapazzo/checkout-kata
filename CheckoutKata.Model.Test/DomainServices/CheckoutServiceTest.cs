7b22afa78053b959d510a4b76ee20a6bf4007089
Matthew Bowen <matthew.bowen@abs.uk.com>
2025-11-07 14:13:52 +0000

    ABS-28 Got discounts unit test working for checkout service

--- CheckoutKata.Model.Test/DomainServices/CheckoutServiceTest.cs
+++ CheckoutKata.Model.Test/DomainServices/CheckoutServiceTest.cs
@@ -57,16 +57,17 @@ public class CheckoutServiceTest
             Sku = "B",
             UnitPrice = 33.00m
         };
-       
+
         var mediatorMock = new Mock<IMediator>();
         var discountRepositoryMock = new Mock<IDiscountRepository>();
         // Arrange
         discountRepositoryMock.Setup(d => d.GetDiscountsAsync()).ReturnsAsync((List<Discount>?)null);
         var productItemRepositoryMock = new Mock<IProductItemRepository>();
 
-        productItemRepositoryMock.Setup(p => p.GetProductItemAsync(It.Is<string>(v => v =="A"))).ReturnsAsync(firstProductItem);
+        productItemRepositoryMock.Setup(p => p.GetProductItemAsync(It.Is<string>(v => v == "A")))
+            .ReturnsAsync(firstProductItem);
         productItemRepositoryMock.Setup(p => p.GetProductItemAsync("B")).ReturnsAsync(secondProductItem);
-        
+
         var checkoutService = new CheckoutService(mediatorMock.Object, discountRepositoryMock.Object,
             productItemRepositoryMock.Object);
         // Act
@@ -76,6 +77,113 @@ public class CheckoutServiceTest
         var totalPrice = await checkoutService.GetTotalPrice();
         // Assert
         Assert.Equal(133.00m, totalPrice);
+    }
+
+    /// <summary>
+    ///     Confirms that when GetTotalPrice is called and there is a discount then it returns the discounted basket
+    /// </summary>
+    [Fact]
+    public async Task CheckoutService_GetTotalPrice_DiscountExists_DiscountApplied()
+    {
+        var firstProductItem = new ProductItem
+        {
+            Sku = "A",
+            UnitPrice = 50.00m
+        };
+        var secondProductItem = new ProductItem
+        {
+            Sku = "B",
+            UnitPrice = 30.00m
+        };
+        var thirdProductItem = new ProductItem
+        {
+            Sku = "C",
+            UnitPrice = 20.00m
+        };
+        var fourthProductItem = new ProductItem
+        {
+            Sku = "D",
+            UnitPrice = 15.00m
+        };
+
+        var discount1 = new BuyXOnePriceDiscount
+        {
+            Name = "A 3 for 130"
+        };
+
+        var discount2 = new BuyXOnePriceDiscount
+        {
+            Name = "B 2 for 45"
+        };
+
+        var mediatorMock = new Mock<IMediator>();
+        mediatorMock.Setup(m =>
+                m.Send(It.Is<BuyXOnePriceDiscount>(d => d.Name == "A 3 for 130"), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(
+                new DiscountResponse
+                {
+                    ProductItems = new List<ProductItem>
+                    {
+                        new()
+                        {
+                            Sku = Guid.NewGuid().ToString(),
+                            UnitPrice = 130.00m
+                        },
+                        secondProductItem,
+                        secondProductItem,
+                        thirdProductItem,
+                        fourthProductItem
+                    }
+                });
+        mediatorMock.Setup(m =>
+                m.Send(It.Is<BuyXOnePriceDiscount>(d => d.Name == "B 2 for 45"), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(
+                new DiscountResponse
+                {
+                    ProductItems = new List<ProductItem>
+                    {
+                        new()
+                        {
+                            Sku = Guid.NewGuid().ToString(),
+                            UnitPrice = 130.00m
+                        },
+                        new()
+                        {
+                            Sku = Guid.NewGuid().ToString(),
+                            UnitPrice = 45.00m
+                        },
+                        thirdProductItem,
+                        fourthProductItem
+                    }
+                });
+        var discountRepositoryMock = new Mock<IDiscountRepository>();
+        // Arrange
+        discountRepositoryMock.Setup(d => d.GetDiscountsAsync()).ReturnsAsync(new List<Discount>
+        {
+            discount1,
+            discount2
+        });
+        var productItemRepositoryMock = new Mock<IProductItemRepository>();
+
+        productItemRepositoryMock.Setup(p => p.GetProductItemAsync(It.Is<string>(v => v == "A")))
+            .ReturnsAsync(firstProductItem);
+        productItemRepositoryMock.Setup(p => p.GetProductItemAsync("B")).ReturnsAsync(secondProductItem);
+        productItemRepositoryMock.Setup(p => p.GetProductItemAsync("C")).ReturnsAsync(thirdProductItem);
+        productItemRepositoryMock.Setup(p => p.GetProductItemAsync("D")).ReturnsAsync(fourthProductItem);
+
+        var checkoutService = new CheckoutService(mediatorMock.Object, discountRepositoryMock.Object,
+            productItemRepositoryMock.Object);
+        // Act
+        await checkoutService.ScanAsync("A");
+        await checkoutService.ScanAsync("B");
+        await checkoutService.ScanAsync("A");
+        await checkoutService.ScanAsync("A");
+        await checkoutService.ScanAsync("B");
+        await checkoutService.ScanAsync("C");
+        await checkoutService.ScanAsync("D");
+        var totalPrice = await checkoutService.GetTotalPrice();
+        // Assert
+        Assert.Equal(210.00m, totalPrice);
     }
 
     #endregion
